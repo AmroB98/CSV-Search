7e51e738b63feea136e830db3dc0ab1bdb4e2d41
Amro Batwa <72704893+AmroB98@users.noreply.github.com>
2022-07-04 20:12:20 +0300

    Update csvfind.py

--- csvfind.py
+++ csvfind.py
@@ -1,24 +1,59 @@
-import glob
-import pandas as pd
+import csv
 import os
 import sys
+import glob
+import time
+import random
 
-path = sys.argv[0]
-# check if path is valid
-if not (os.path.isfile(path)):
-    print("Given path is not valid")
-    quit("program terminated")
-# loop over all CSV files in directory
-files = glob.glob(path + "/*.csv")
+mylist = []
+# for i in range (700):
+#     mylist.append(random.randrange(100,200))
+# get command line arguments
+path = sys.argv[1]
+target_column = sys.argv[2]
+target_str = sys.argv[3]
+mylist = []
 
-# store data
-frame = pd.DataFrame()
-content = []
+# set path
+os.chdir(path)
+isFolder = os.path.exists("output")
+if not isFolder:
+    os.mkdir("output")
+if not os.path.exists("data"):
+    os.mkdir("data")
+if not os.path.exists("list"):
+    os.mkdir("list")
 
-for filename in files:
-    df = pd.read_csv(filename, index_col=None)
-    content.append(df)
+if os.path.exists('output/output.csv'):
+    inp = input("output.csv file already exist in output folder, Do you want to overwrite it? [Y/N]")
+    if inp == "Y" or inp == "y":
+        os.remove("output/output.csv")
+    else:
+        quit("Terminated.")
 
-# convert it to pandas matrix
-data_frame = pd.concat(content)
-print(data_frame)
+start_time = time.time()
+with open('list/list.csv', 'r') as infile:
+    #infile_object = csv.reader(infile)
+    reader = csv.reader(infile)
+    for rows in reader:
+        print(rows[0])
+        mylist.append(rows[0])
+
+files = glob.glob('data/*.csv')
+count = 0
+for file in files:
+    print("Proccessing file: " + file)
+    with open(file) as cf:
+        ob = csv.reader(cf)
+
+        for rows in ob:
+            for i in mylist:
+                if rows[int(target_column)] == str(i).casefold():
+                    count += 1
+                    with open('output/output.csv', 'a', newline="") as outfile:
+                        print("writing target row #" + str(count))
+                        writer = csv.writer(outfile)
+                        writer.writerow(rows)
+                        outfile.close()
+print("Total Targets found: " + str(count))
+print("Execution time:  %.2f seconds " % (time.time() - start_time))
